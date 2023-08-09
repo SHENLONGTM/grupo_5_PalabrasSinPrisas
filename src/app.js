c06706fbdb87eecab3ee7a474a112d308739e151
mariavillalba06 <mariadeangeles131@gmail.com>
2023-08-09 10:45:51 -0300

    creacion de productDetail.html

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 //Poner a escuchar al servidor
 app.listen(3000,()=>{
-    console.log("Servidor corriendo");
+    console.log("Servidor corriendo en http://localhost:3000/");
 })
 
 app.use(express.static('public'));
